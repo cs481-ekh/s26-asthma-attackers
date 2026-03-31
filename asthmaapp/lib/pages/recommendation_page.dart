441a00fb825c4e7a3209b15961317047a6dd7a73
Cameron Quitugua <cameronquitugua@u.boisestate.edu>
2026-03-30 23:44:13 -0600

    Remove unused line

--- asthmaapp/lib/pages/recommendation_page.dart
+++ asthmaapp/lib/pages/recommendation_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter/gestures.dart';
 
 import '../app_theme.dart';
 import '../models/aqi_result.dart';
